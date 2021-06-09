fff6b2d9483eba730abfcb2408fb9bbe17725a99
Woosang <yws1502@naver.com>
2021-06-10 01:18:17 +0900

    SaBuJak

---
+++ Web/SaBuJak/assets/js/mask_detect.js
@@ -0,0 +1,40 @@
+const video = document.querySelector('video');
+const img = document.querySelector('img');
+
+// play webcam
+navigator.mediaDevices.getUserMedia({video: {width: 500, height: 281}})
+.then((stream) => video.srcObject = stream);
+
+const getFrame = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    const data = canvas.toDataURL('image/png');
+    return data;
+}
+
+const WS_URL = 'ws://localhost:5500/ws';   // <<<<<<< 이 부분 port번호 맞추기
+const FPS = 3;
+const ws = new WebSocket(WS_URL);
+
+ws.onopen = () => {
+    console.log(`Connected to ${WS_URL}`);
+    setInterval(() => {
+        ws.send(getFrame());
+    }, 1000 / FPS);
+}
+
+ws.onmessage = message => {
+    var msg = message.data;
+
+    img.src = msg.substring(0, msg.length -3);
+
+    var bar = document.getElementById('myBar');
+
+    dangerRate = msg.substring(msg.length -3);
+
+    bar.style.width = dangerRate + '%';
+
+    document.getElementById('label').innerHTML = dangerRate * 1 + '%'
+}
